8614b505aefbe98b22d6ac30befabc59979b7176
Maaack <marek.belski@gmail.com>
2023-10-18 22:54:33 -0700

    Cleans up crop pickup.

--- Scenes/Pickups/CropPickup.gd
+++ Scenes/Pickups/CropPickup.gd
@@ -2,9 +2,6 @@
 extends Pickup
 class_name CropPickup
 
-const WHEAT_FRAME = 5
-const EGGPLANT_FRAME = 11
-
 var item : BaseQuantity
 
 @export var crop_type : Constants.Crops = Constants.Crops.NONE :
@@ -13,11 +10,10 @@ var item : BaseQuantity
 		if is_visible_in_tree():
 			match(crop_type):
 				Constants.Crops.WHEAT:
-					$Sprite2D.frame = WHEAT_FRAME
 					item = load("res://Resources/Items/HarvestedWheat.tres")
 				Constants.Crops.EGGPLANT:
-					$Sprite2D.frame = EGGPLANT_FRAME
 					item = load("res://Resources/Items/HarvestedEggplant.tres")
+			$Sprite2D.texture = item.icon
 
 func _ready():
 	crop_type = crop_type
